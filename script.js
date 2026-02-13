9b022d195c8f9b1860c0a331860a88731bbf4131
Ohveeff <aingunj@gmail.com>
2026-02-12 22:52:23 -0600

    Update script.js

--- script.js
+++ script.js
@@ -16,7 +16,7 @@ document.addEventListener("DOMContentLoaded", () => {
   document.getElementById("toggle-inv-btn").onclick = () =>
     document.getElementById("inventory").classList.toggle("hidden");
   document.getElementById("add-coins-btn").onclick = () => {
-    coins += 10.00;
+    coins += 10.01;
     updateCoins();
   };
   document.getElementById("remove-coins-btn").onclick = () => {
